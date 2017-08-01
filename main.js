b5440da786510b1ca1654fb2e86bb2135ff81a34
radhikasah <sahradhika14@gmail.com>
2017-08-01 06:18:43 -0700

    modify the restaurants

--- main.js
+++ main.js
@@ -23,6 +23,44 @@ foodieApp.config(function ($routeProvider) {
 
 foodieApp.controller('restaurantController',function($scope,$routeParams) {
 	$scope.restaurantId = $routeParams.id;
+	var restaurants = [{
+	name: 'Farzi Cafe',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	id:1,
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBJPEXGlAtA-eWhyGOjSEVV3gVY0IT8ywTL9eH79uo3AYCclPI'
+	
+},
+{
+	name: 'Club Coffee',
+	address: 'Baddi',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.5',
+	cuisines: 'Modern Indian',
+	cost: '200',
+	id:2,
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoKh-sAKBzmI4Shgz4QVLO643FZ7SZDEJd0fb69rPixmIFnsur'
+},
+{
+	name: 'Aroma Mocha',
+	address: 'Janakpur',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.7',
+	cuisines: 'Modern Nepal',
+	cost: '700',
+	id:3,
+	hours: '12 Noon to 1 AM (sun-Sat)',
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtCTNtWjF54uFAJR4mxuOVP3j8Xf-TflcgO5H0p-XWU8QJxecQsA'
+}]
+	$scope.restaurant = restaurants[$routeParams.id - 1];
 	
 //console.log(foodieApp);
 
